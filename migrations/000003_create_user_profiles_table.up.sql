e96230c4dc9a0fd8d9c247e720948c29ab0d735f
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-03 14:06:21 +0700

    Feat: Creat file migrate for user_profiles schema

---
+++ migrations/000003_create_user_profiles_table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE user_profiles (
+    id INT PRIMARY KEY,
+    user_id INT NOT NULL,
+    full_name VARCHAR(255),
+    phone VARCHAR(20),
+    address TEXT,
+    photo_url VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
