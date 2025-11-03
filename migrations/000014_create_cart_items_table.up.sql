3a724440e77aa548fccd342c57f0f0fa19843739
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-03 14:27:19 +0700

    Feat: Create migration files for cart_items schema

---
+++ migrations/000014_create_cart_items_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE cart_items (
+    id INT PRIMARY KEY,
+    user_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL DEFAULT 1,
+    size_id INT REFERENCES product_sizes(id),
+    temperature_id INT REFERENCES product_temperatures(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
